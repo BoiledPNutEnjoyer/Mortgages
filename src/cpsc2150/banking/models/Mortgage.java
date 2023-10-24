a33c0c3b6c6281cf8a6a4542de31dd1a246933f2
BoiledPNutEnjoyer <jbecke3@clemson.edu>
2023-10-24 16:29:44 -0400

    fixed it
    x

--- src/cpsc2150/banking/models/Mortgage.java
+++ src/cpsc2150/banking/models/Mortgage.java
@@ -1,1 +1,35 @@
-//
+package cpsc2150.banking.models;
+public class Mortgage extends AbsMortgage implements IMortgage{
+    public Mortgage(double houseCost, double downPayment, int years, Customer customer) {
+    }
+
+    @Override
+    public double getPayment() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPayment'");
+    }
+
+    @Override
+    public double getRate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRate'");
+    }
+
+    @Override
+    public double getPrincipal() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPrincipal'");
+    }
+
+    @Override
+    public int getYears() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getYears'");
+    }
+
+    @Override
+    public boolean loanApproved() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loanApproved'");
+    }
+}
